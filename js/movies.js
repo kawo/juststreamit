e66f36dcbadddaacfacd37e9b7a37266b18d0324
Kévin Audebrand <kevin.audebrand@gmail.com>
2021-09-28 15:27:39 +0200

    pagination: promise.all

--- js/movies.js
+++ js/movies.js
@@ -20,36 +20,35 @@ document.addEventListener("DOMContentLoaded", () => {
         return pages_number;
     };
 
-    async function fetchMovies(number_of_movies = 1, categorie = "all") {
+    async function fetchMovies(url, number_of_movies = 1) {
         let movie_url;
         let pages_number = apiPagination(number_of_movies);
         console.log("fetchMovie number of pages: ");
         console.log(pages_number);
-        var movie;
+        var movie = [];
         var movies;
-        if (categorie == "all") {
-            let best_movies_url = "http://localhost:8000/api/v1/titles?sort_by=-imdb_score&page=1";
-            if (number_of_movies < 5) {
-                movies = fetch(best_movies_url)
-                    .then((data) => data.json())
-                    .then((data) => {
-                        for (var i = 0; i < number_of_movies; i++) {
-                            console.log("fetchBestMovies loop number: " + i);
-                            console.log(data);
-                            movie_url = data.results[i].url;
-                            movie = fetch(movie_url)
-                                .then((data) => data.json())
-                                .then(movie => {
-                                    console.log("fetchBestMovies movie: ");
-                                    console.log(movie);
-                                    return movie;
-                                });
-                        };
-                    });
-                await movies;
-                return movie;
-            };
+        if (number_of_movies < 5) {
+            movies = fetch(url)
+                .then((data) => data.json())
+                .then((data) => {
+                    for (var i = 0; i < number_of_movies; i++) {
+                        console.log("fetchMovies loop number: " + i);
+                        console.log(data);
+                        movie_url = data.results[i].url;
+                        movie.push(fetch(movie_url)
+                            .then((data) => data.json())
+                            .then(movie => {
+                                console.log("fetchMovies loop movie: " + i);
+                                console.log(movie);
+                                return movie;
+                            }));
+                    };
+                });
         };
+        await movies;
+        console.log("fetchMovies movie: ");
+        console.log(movie);
+        return movie;
     };
 
     function bestMovieAllTime() {
@@ -57,30 +56,55 @@ document.addEventListener("DOMContentLoaded", () => {
         let bm_desc = document.getElementById("bm_desc");
         let bm_img = document.getElementById("bm_img");
         let bm_button = document.getElementById("bm_button");
-        let best_movie = fetchMovies();
-        console.log("bestMovieAllTime: ");
-        console.log(best_movie);
-        best_movie.then((movie) => {
-            console.log("bestMovieAllTime result: ");
-            console.log(best_movie);
-            bm_title.innerHTML = movie.title;
-            bm_desc.innerHTML = movie.description;
-            bm_img.src = movie.image_url;
-            bm_button.onclick = () => {
-                let bm_modal = new Modal(movie);
-                bm_modal.modal;
-                modal.style.display = "block";
-            };
-        })
-        best_movie.catch(function(error) {
-            console.log(error);
-        });
+        let best_movie = fetchMovies("http://localhost:8000/api/v1/titles?sort_by=-imdb_score&page=1")
+            .then((movies) => {
+                Promise.all(movies).then(movie => {
+                    bm_title.innerHTML = movie[0].title;
+                    bm_desc.innerHTML = movie[0].description;
+                    bm_img.src = movie[0].image_url;
+                    bm_button.onclick = () => {
+                        let bm_modal = new Modal(movie[0]);
+                        bm_modal.modal;
+                        modal.style.display = "block";
+                    };
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
     };
 
     function bestSevenMovies() {
-        let best_movies = fetchMovies(5);
-        console.log("bestSevenMovies: ");
-        console.log(best_movies);
+        let img_carousel1 = document.getElementById("img_carousel1");
+        let img_carousel2 = document.getElementById("img_carousel2");
+        let img_carousel3 = document.getElementById("img_carousel3");
+        let img_carousel4 = document.getElementById("img_carousel4");
+        let toprated_movies = fetchMovies("http://localhost:8000/api/v1/titles?sort_by=-imdb_score&page=1", 4)
+            .then((movies) => {
+                Promise.all(movies).then(movie => {
+                    img_carousel1.src = movie[1].image_url;
+                    img_carousel1.onclick = () => {
+                        let tr_modal = new Modal(movie[1]);
+                        tr_modal.modal;
+                        modal.style.display = "block";
+                    };
+                    img_carousel2.src = movie[2].image_url;
+                    img_carousel2.onclick = () => {
+                        let tr_modal = new Modal(movie[2]);
+                        tr_modal.modal;
+                        modal.style.display = "block";
+                    };
+                    img_carousel3.src = movie[3].image_url;
+                    img_carousel3.onclick = () => {
+                        let tr_modal = new Modal(movie[3]);
+                        tr_modal.modal;
+                        modal.style.display = "block";
+                    };
+                });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
     };
 
     bestMovieAllTime();
